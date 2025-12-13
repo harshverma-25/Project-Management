0be5841556297d4f2a80f9e2337681f041e6fa66
Harsh Verma <hv.harshverma025@gmail.com>
2025-12-13 09:37:57 +0530

    Add Hash Password hooks and Compare pass methods

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose  from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
     avatar:{
@@ -57,6 +58,24 @@ const userSchema = new mongoose.Schema({
     
 }, {timestamps: true
 });
+
+// hashing password before saving user
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")){
+        return next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+// method to compare passwords
+userSchema.methods.isPasswordMatch = async function(password){
+    return await bcrypt.compare(password, this.password);
+};
+
+
+
+
 
 const User = mongoose.model("User", userSchema);
 export default User;
